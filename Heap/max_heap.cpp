05185d01a27ff1598762766ee0d078fe183d5c53
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-22 14:35:00 +0800

    Update max_heap.cpp

--- Heap/max_heap.cpp
+++ Heap/max_heap.cpp
@@ -12,7 +12,7 @@ struct MAX_HEAP {
 
 
 
- //堆的创建
+//堆的创建
 Heap* creat_heap(Heap* heap) 
 {
 	Heap* heap = (Heap*)malloc(sizeof(Heap));
@@ -31,25 +31,25 @@ void downHeapify(Heap* heap, int index)
 	if (index >= heap->count)      
 		return;
 
-	int left = index * 2 + 1;      //  index的左子节点
-	int right = index * 2 + 2;     //  index的右子节点
+	int left = index * 2 + 1;                                   //  index的左子节点
+	int right = index * 2 + 2;                                  //  index的右子节点
 	
 	int maximun = (*((heap->p) + index));
 
 	if (left < heap->count || maximun < (*((heap->p) + left)))  //父节点的值比左值小
 	{
-		maximun = (*((heap->p) + left));						//交换父节点和左子节点的值
+		maximun = (*((heap->p) + left));		    //交换父节点和左子节点的值
 		*((heap->p) + left) = *((heap->p) + index);    
 		*((heap->p) + index) = maximun;
-		downHeapify(heap, left);								//再把新左子节点变为父节点进行下沉
+		downHeapify(heap, left);			    //再把新左子节点变为父节点进行下沉
 
 	}
-	if (right < heap->count || maximun < (*(heap->p) + right))	//根节点的值比左值大，比右值小
+	if (right < heap->count || maximun < (*(heap->p) + right))  //根节点的值比左值大，比右值小
 	{
-		maximun = (*(heap->p) + right);							//交换父节点和右节点的值
+		maximun = (*(heap->p) + right);			    //交换父节点和右节点的值
 		*((heap->p) + right) = *((heap->p) + index);
 		*((heap->p) + index) = maximun;
-		downHeapify(heap, right);								//把新的右子节点变为父节点进行下沉	
+		downHeapify(heap, right);			    //把新的右子节点变为父节点进行下沉	
 	}
 }
 
@@ -63,10 +63,10 @@ void upHeapify(Heap* heap, int index)
 
 	if (*((heap->p) + index) > * ((heap->p) + parent))        //如果元素比父节点大就进行上浮
 	{
-		int temp = *((heap->p) + index);					  //把父节点和自身进行交换
+		int temp = *((heap->p) + index);		  //把父节点和自身进行交换
 		*((heap->p) + index) = *((heap->p) + parent);
 		*((heap->p) + parent) = temp;
-		upHeapify(heap, parent);                              //再把新的父节点进行新的一轮上浮
+		upHeapify(heap, parent);                           //再把新的父节点进行新的一轮上浮
 	}
 }
 
@@ -87,7 +87,7 @@ void push(Heap* heap, int x)
 		heap->p = (int * )realloc(heap->p, heap->size * sizeof(int));
 
 	}
-	upHeapify(heap, heap->count - 1);							//把新插入的元素上浮到合适的位置
+	upHeapify(heap, heap->count - 1);						//把新插入的元素上浮到合适的位置
 }
 
 
@@ -102,7 +102,7 @@ void pop(Heap* heap)
 	int temp = *((heap->p) + heap->count);						//把取出的元素和根节点进行交换
 	*((heap->p) + heap->count) = *(heap->p);
 	*(heap->p) = temp;
-	downHeapify(heap, 0);										///再把0下沉到合适的位置
+	downHeapify(heap, 0);								//再把0下沉到合适的位置
 	if (4 * heap->count <=  heap->size)
 	{
 		heap->size /= 2;
@@ -149,17 +149,17 @@ void downHheapify(Heap* heap, int index)
 
 	if (left<heap->count && minimum>* ((heap->p) + left) && minimum < *((heap->p)+right))   //父节点比左子值大比右值小
 	{	
-		minimum = *((heap->p) + left);						//父节点和左子节点进行值交换			
+		minimum = *((heap->p) + left);						        //父节点和左子节点进行值交换			
 		*((heap->p) + left) = *((heap->p) + index);
 		*((heap->p) + index) = minimum;
 		downHeapify(heap, left);							//把新的父节点再进行下沉
 	}
-	else if (right<heap->count && minimum>* ((heap->p) + right))	//	父节点比右子节点值大
+	else if (right<heap->count && minimum>* ((heap->p) + right))	                        //父节点比右子节点值大
 	{
-		minimum = *((heap->p) + right);								//交换值
+		minimum = *((heap->p) + right);							//交换值
 		*((heap->p) + right) = *((heap->p) + index);
 		*((heap->p) + index) = minimum;
-		downHeapify(heap, right);									//把新的父节点再进行下沉
+		downHeapify(heap, right);							//把新的父节点再进行下沉
 	}
 	
 	
