38bc190564dc7a781b10bf6d6013642ae9f2df70
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-24 00:29:35 +0800

    Update 梳排序(combSort).cpp

--- Sroting/梳排序(combSort).cpp
+++ Sroting/梳排序(combSort).cpp
@@ -5,13 +5,13 @@
 void combSort(int* numbers, int size)
 {
 	int gap = size;
-	while (gap > 1)								   //直到不能再分组为止
+	while (gap > 1)						          //直到不能再分组为止
 	{
 		gap = gap / SHRINK;
 		int i = 0;
 		while ((i+gap )< size)					  //把第一组和第二组的元素进行全部比较
 		{
-			if (numbers[i] > numbers[i + gap])    //第一组和第二组的元素进行比较，第一组
+			if (numbers[i] > numbers[i + gap])                //第一组和第二组的元素进行比较，第一组
 			{
 				int tmp = numbers[i];			  //第一组比第二组大就进行交换
 				numbers[i] = numbers[i + gap];
