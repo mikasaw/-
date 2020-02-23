8023d2afe93540ac9ceeb8321b16f45341359b9f
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-23 15:04:07 +0800

    Update 珠排序(beadSort).cpp

--- Sroting/珠排序(beadSort).cpp
+++ Sroting/珠排序(beadSort).cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+
 void beadSort(int* a, int len)			  //a -- 要排序的数组   len--为数组的大小
 {
 	int i, j, max, sum;
@@ -14,7 +15,7 @@ void beadSort(int* a, int len)			  //a -- 要排序的数组   len--为数组的
 
 	beads = static_cast<unsigned char*>(calloc(1, max * len));
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len; i++)                 //把数组中的每个树都初始化为一串珠子，一个珠子代表1；             
 	{
 		for (j = 0; j < a[i]; j++)
 		{
@@ -22,7 +23,7 @@ void beadSort(int* a, int len)			  //a -- 要排序的数组   len--为数组的
 		}
 	}
 
-	for (j = 0; j < max; j++)
+	for (j = 0; j < max; j++)                  //让珠子往下落
 	{
 		for (sum = i = 0; i < len; i++)
 		{
@@ -32,7 +33,7 @@ void beadSort(int* a, int len)			  //a -- 要排序的数组   len--为数组的
 		for (i = len - sum; i < len; i++)
 			BEAD(i, j) = 1;
 	}
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len; i++) 		   //下边有支点就保存不动
 	{
 		for (j = 0; j < max && BEAD(i, j); j++)
 			a[i] = j;
