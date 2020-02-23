d9cb7d816383e75e76402a3806c28b6194e1a9cc
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-24 00:35:39 +0800

    Update 地精排序(gnomeSort).cpp

--- Sroting/地精排序(gnomeSort).cpp
+++ Sroting/地精排序(gnomeSort).cpp
@@ -1,13 +1,13 @@
 
 
 
-//地精排序  
+//地精排序:前进冒泡,一旦遇到冒泡的情况发生就往回冒,直到把这个数字放好为止.
 void gnomeSort(int* numbers, int size)
 {
-	int i = 0;                               // 当i为0时，先让其自增1，再进行排序
+	int i = 0;                                      // 当i为0时，先让其自增1，再进行排序
 	while (i < size)
 	{
-		if (numbers[i] >= numbers[i - 1])    //不发生交换i 就自增1
+		if (numbers[i] >= numbers[i - 1])       //不发生交换i 就自增1
 			i++;
 		else
 		{
@@ -17,6 +17,6 @@ void gnomeSort(int* numbers, int size)
 			i--;
 		}
 		if (i == 0)
-			i = 1;
+		i = 1;
 	}
 }
