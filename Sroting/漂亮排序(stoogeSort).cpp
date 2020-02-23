b2099daddd2c2090dbcb12e983db8091d6530471
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-23 23:57:58 +0800

    Update 漂亮排序(stoogeSort).cpp

--- Sroting/漂亮排序(stoogeSort).cpp
+++ Sroting/漂亮排序(stoogeSort).cpp
@@ -12,7 +12,7 @@ void stoogeSort(int arr[], int i, int j)
 	if ((i + 1) >= j)
 		return;
 	int k = static_cast<int >((j - i + 1) / 3); 
-	stoogeSort(arr, i, j - k);              //先排序前2/3部分
+	stoogeSort(arr, i, j - k);              		      //先排序前2/3部分
 	stoogeSort(arr, i + k, j);				      //再排序后2/3部分
 	stoogeSort(arr, i , j - k);				      //最后进行前面1/3的排序
 }
