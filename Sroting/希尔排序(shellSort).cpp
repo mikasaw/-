d3a9bce686ce32e4dec04cd638460e85e9e346ae
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-24 00:53:23 +0800

    Create 希尔排序(shellSort).cpp

---
+++ Sroting/希尔排序(shellSort).cpp
@@ -0,0 +1,16 @@
+
+
+void shellSort(int array[], int len)
+{
+	int i, j, gap;
+
+	for (gap = len / 2; gap > 0; gap = gap / 2)
+		for (i = gap; i < len; i++)
+			for (j = i - gap; j >= 0 && array[j] > array[j + gap]; j = j - gap)
+					{
+						int temp = array[j];
+						array[j] = array[j + gap];
+						array[j + gap] = temp;
+ 					}
+
+}
