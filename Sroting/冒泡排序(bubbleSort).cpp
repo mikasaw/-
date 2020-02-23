add04e1266ad836a2a5f2bc545a99ed35ca3eb64
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-23 15:56:09 +0800

    Update 冒泡排序(bubbleSort).cpp

--- Sroting/冒泡排序(bubbleSort).cpp
+++ Sroting/冒泡排序(bubbleSort).cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+void bubbleSort(int arr[], int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		for(int j =0;j<size-1;i++)
+			if (arr[i] > arr[j + 1])
+			{
+				int temp = arr[i];
+				arr[i] = arr[j + 1];
+				arr[j + 1] = temp;
+	  	}
+	}
+}
+
 
