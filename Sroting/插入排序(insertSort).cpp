9dcd8454da592c7627b626ced40bf1be226c220b
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-24 00:38:39 +0800

    Update 插入排序(insertSort).cpp

--- Sroting/插入排序(insertSort).cpp
+++ Sroting/插入排序(insertSort).cpp
@@ -7,11 +7,11 @@ void insertionSort(int arr[], int size)
 	{
 		j = i - 1;
 		key = arr[i];
-		while (j >= 0 && key <arr[j])      //遇到小的就把元素后移，在把值插入到前面去
+		while (j >= 0 && key <arr[j])      //遇到小的就把元素后移
 		{
 			arr[j + 1] = arr[j];
 			j = j - 1;
 		}
-		arr[j + 1] = key;
+		arr[j + 1] = key;		   //把值插入到空出来位置
 	}
 }
