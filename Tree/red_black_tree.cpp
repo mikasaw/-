032b0458a36972731839d54e40aca14ddb2a5cd6
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-23 10:58:06 +0800

    Update red_black_tree.cpp

--- Tree/red_black_tree.cpp
+++ Tree/red_black_tree.cpp
@@ -1,14 +1,14 @@
 
 
-//�����������:
-//��1��ÿ���ڵ�����Ǻ�ɫ�������Ǻ�ɫ��
-//��2�����ڵ��Ǻ�ɫ��
-//��3��ÿ��Ҷ�ӽڵ㣨NIL���Ǻ�ɫ��[ע�⣺����Ҷ�ӽڵ㣬��ָΪ��(NIL��NULL)��Ҷ�ӽڵ㣡]
-//��4�����һ���ڵ��Ǻ�ɫ�ģ��������ӽڵ�����Ǻ�ɫ�ġ�
-//��5����һ���ڵ㵽�ýڵ������ڵ������·���ϰ�����ͬ��Ŀ�ĺڽڵ㡣
+//红黑树的特性:
+//（1）每个节点或者是黑色，或者是红色。
+//（2）根节点是黑色。
+//（3）每个叶子节点（NIL）是黑色。[注意：这里叶子节点，是指为空(NIL或NULL)的叶子节点！]
+//（4）如果一个节点是红色的，则它的子节点必须是黑色的。
+//（5）从一个节点到该节点的子孙节点的所有路径上包含相同数目的黑节点。
 
-// ����(3)�е�Ҷ�ӽڵ㣬��ֻΪ��(NIL��null)�Ľڵ㡣
-// ����(5)��ȷ��û��һ��·���������·����������������������������ǽӽ�ƽ��Ķ�������
+// 特性(3)中的叶子节点，是只为空(NIL或null)的节点。
+// 特性(5)，确保没有一条路径会比其他路径长出俩倍。因而，红黑树是相对是接近平衡的二叉树。
 
 
 #include<iostream>
@@ -22,10 +22,10 @@ struct Node {
 	Node* par;
 	Node* left;
 	Node* right;
-	int color;        // 0Ϊ��ɫ��1Ϊ��ɫ
+	int color;            // 0为黑色，1为红色
 };
 
-//�����ڵ�
+//创建节点
 Node* newnode(int val, Node* par)
 {
 	//Node* newnode = (Node*)malloc(sizeof(Node));
@@ -34,13 +34,13 @@ Node* newnode(int val, Node* par)
 	newnode->par = par;
 	newnode->left = NULL;
 	newnode->right = NULL;
-	newnode->color = 1;   //��ɫ��ʼ��Ϊ��ɫ
+	newnode->color = 1;   //颜色初始化为红色
 	
 	return newnode;
 
 }
 
-//���һ���ڵ��Ƿ�ΪҶ��
+//检查一个节点是否为叶子
 bool isleaf(Node* n)
 {
 	if (n->left == NULL || n->right == NULL)
@@ -50,28 +50,28 @@ bool isleaf(Node* n)
 }
 
 
-//����
+//左旋
 Node* leftRotate(Node* lnode)
 {
 	
-		Node* parent = lnode->right;                  //����ת�������ӽ����Ϊ�µĸ��״洢
-		Node* grandParent = lnode->par;              //���游����Ϣ�洢
+		Node* parent = lnode->right;                  //把旋转结点的右子结点作为新的父亲存储
+		Node* grandParent = lnode->par;               //把祖父的信息存储
 
 
 		parent->left = lnode;
-		if (lnode->right != NULL)                    //�ѽ���������������Ϊ�游
+		if (lnode->right != NULL)                     //把结点的右子树父亲设为祖父
 			lnode->right->par = grandParent;
 
 		lnode->par = parent;
-		lnode->right = parent->left;                  // ���¸��׵����ӽڵ��ƶ���Ϊ��ת�����ҽ��
+		lnode->right = parent->left;                  // 把新父亲的左子节点移动成为旋转结点的右结点
 		parent->left->par = lnode; 
 			                                          
-		if (grandParent != NULL)                      //�ж��µĸ�����λ��     
+		if (grandParent != NULL)                      //判断新的父结点的位置     
 		{
-			if (grandParent->right == parent)         //����߾ͳ�Ϊ���������ӽ��
+			if (grandParent->right == parent)     //在左边就成为根结点的左子结点
 				grandParent->right = parent;
 			else
-				grandParent->left = parent;           //���ұ߾ͳ�Ϊ���������ӽ�� 
+				grandParent->left = parent;   //在右边就成为根结点的右子结点 
 			
 		}
 	
@@ -82,7 +82,7 @@ Node* leftRotate(Node* lnode)
 
 
 
-//����  ����ͬ��
+//右旋  分析同上
 Node* rightRotate(Node* rnode) 
 {
 	Node* parent = rnode->left;
@@ -108,31 +108,31 @@ Node* rightRotate(Node* rnode)
 }
 
 
-//�ڲ�������������
+//在插入操作后调整树
 void checkNode(Node* node) {
 
-	// �ڵ�Ϊ�����
+	// 节点为根结点
 	if (node == NULL || node->par == NULL) {
 		return;
 	}
 	Node* child = node;
-	// ���Ӷ��Ǻ�ɫ�ڵ�
+	// 父子都是黑色节点
 	if (node->color == 0 || (node->par)->color == 0) {
 		return;
 	}
 
-	// ���Ӷ��Ǻ�ɫ�ڵ�
-	// ����常
+	// 父子都是红色节点
+	// 检查叔父
 	Node* parent = node->par;
 	Node* grandParent = parent->par;
 
-	//үүΪ�գ�����Ϊ���ڵ�
+	//爷爷为空，父亲为根节点
 	if (grandParent == NULL) {
 		parent->color = 0;
 		return;
 	}
 
-	//���Ӻ�үү��Ϊ��ɫ
+	//孩子和爷爷都为黑色
 	if (grandParent->right != NULL && (grandParent->right)->color == 1 
 		&& grandParent->left != NULL && (grandParent->left)->color == 1)
 	{
@@ -142,7 +142,7 @@ void checkNode(Node* node) {
 		return;
 	}
 	else {
-		  //����ߵ����
+		  //在左边的情况
 		Node* greatGrandParent = grandParent->par;
 		if (grandParent->right == parent) {
 			
@@ -165,12 +165,12 @@ void checkNode(Node* node) {
 					}
 				}
 
-				// �ı���ɫ
+				// 改变颜色
 				parent->color = 0;
 				grandParent->color = 1;
 			}
 			else {   
-				// ���׺ͺ�����ת
+				// 父亲和孩子旋转
 				parent->left = child->right;
 				if (child->right != NULL) {
 					(child->right)->par = parent;
@@ -178,7 +178,7 @@ void checkNode(Node* node) {
 				child->right = parent;
 				parent->par = child;
 
-				// ���Ӻ��游��ת
+				// 孩子和祖父旋转
 				grandParent->right = child->left;
 				if (child->left != NULL) {
 					(child->left)->par = grandParent;
@@ -186,7 +186,7 @@ void checkNode(Node* node) {
 				child->left = grandParent;
 				grandParent->par = child;
 
-				// ���ӵ�����
+				// 附加到树上
 				child->par = greatGrandParent;
 				if (greatGrandParent != NULL) {
 					if (greatGrandParent->left != NULL && greatGrandParent->left == grandParent) {
@@ -197,12 +197,12 @@ void checkNode(Node* node) {
 					}
 				}
 
-				// �ı亢�Ӻ��游��ɫ
+				// 改变孩子和祖父颜色
 				child->color = 0;
 				grandParent->color = 1;
 			}
 		}
-		else {   //������
+		else {   //左边情况
 			
 			if (parent->left == node) {
 				grandParent->left = parent->right;
@@ -212,7 +212,7 @@ void checkNode(Node* node) {
 				parent->right = grandParent;
 				grandParent->par = parent;
 
-				// ���ӵ�����
+				// 附加到树上
 				parent->par = greatGrandParent;
 				if (greatGrandParent != NULL) {
 					if (greatGrandParent->left != NULL && greatGrandParent->left == grandParent) {
@@ -223,13 +223,13 @@ void checkNode(Node* node) {
 					}
 				}
 
-				// �ı亢�Ӻ��游����ɫ
+				// 改变孩子和祖父的颜色
 				parent->color = 0;
 				grandParent->color = 1;
 			}
 			else {   //Left Right Case
 
-				// ��ת���׺ͺ���
+				// 旋转父亲和孩子
 				parent->right = child->left;
 				if (child->left != NULL) {
 					(child->left)->par = parent;
@@ -237,7 +237,7 @@ void checkNode(Node* node) {
 				child->left = parent;
 				parent->par = child;
 
-				// ��ת���Ӻ��游
+				// 旋转孩子和祖父
 				grandParent->left = child->right;
 				if (child->right != NULL) {
 					(child->right)->par = grandParent;
@@ -245,7 +245,7 @@ void checkNode(Node* node) {
 				child->right = grandParent;
 				grandParent->par = child;
 
-				// ���ӵ�����
+				// 附加到树上
 				child->par = greatGrandParent;
 				if (greatGrandParent != NULL) {
 					if (greatGrandParent->left != NULL && greatGrandParent->left == grandParent) {
@@ -256,7 +256,7 @@ void checkNode(Node* node) {
 					}
 				}
 
-				// �ı亢�Ӻ��游����ɫ
+				// 改变孩子和祖父的颜色
 				child->color = 0;
 				grandParent->color = 1;
 			}
@@ -267,11 +267,11 @@ void checkNode(Node* node) {
 
 
 
-//�����ϲ�����
+//在树上插入结点
 void insertNode(int val, Node* root)
 {
 	Node* buffRoot = root;
-	while (buffRoot)                   //�ݹ��ҵ����ʵ�λ�á�
+	while (buffRoot)                  				  //递归找到合适的位置。
 	{
 		if (buffRoot->val > val)
 		{
@@ -279,7 +279,7 @@ void insertNode(int val, Node* root)
 				buffRoot = buffRoot->left;
 			else
 			{
-				buffRoot->left = newnode(val, buffRoot);   //�Ҳ����򴴽��µĽ��
+				buffRoot->left = newnode(val, buffRoot);   //找不到则创建新的结点
 				break;
 			}
 		}
@@ -297,7 +297,7 @@ void insertNode(int val, Node* root)
 	}
 	while (buffRoot != root)          
 	{
-		checkNode(buffRoot);                  //�Բ������������е���
+		checkNode(buffRoot);                 			 //对插入结点后的树进行调整
 		if (buffRoot->par = NULL)
 		{
 			root = buffRoot;
@@ -310,7 +310,7 @@ void insertNode(int val, Node* root)
 	}
 }
 
-//���õݹ���������(��ɾ��������)
+//采用递归来调整树(在删除操作后)
 void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 
 	if (toDelete == (*root)) {
@@ -334,11 +334,11 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 	}
 
 
-	// ��ȡ������ݵ���Ϣ�����н�һ������
+	// 获取结点亲戚的信息来进行进一步调整
 	Node* sibling;
 	Node* parent = toDelete->par;
-	int locateChild = 0;    // ��������Ľ��ΪҶ�ӽ����Ϊ0�������Ϊ 1.
-	if (parent->right == toDelete) {     //��ȡToDelete�����ֵܡ�
+	int locateChild = 0;    		 // 如果调整的结点为叶子结点则为0，父结点为 1.
+	if (parent->right == toDelete) {    	 //获取ToDelete结点的兄弟。
 		sibling = parent->left;
 		locateChild = 1;
 	}
@@ -346,32 +346,32 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 		sibling = parent->right;
 	}
 
-	//����ֵܽ����ӽ�㶼Ϊ��ɫ
+	//如果兄弟结点的子结点都为红色
 	if ((sibling->right != NULL && sibling->right->color == 1) || (sibling->left != NULL && sibling->left->color == 1)) {
 		if (sibling->right != NULL && sibling->right->color == 1) {
 
-			// �ֵ�Ϊ���׵����ӽ�㣬�ֵܵ��ӽ��Ϊ�ҽ�� 
+			// 兄弟为父亲的左子结点，兄弟的子结点为右结点 
 			if (locateChild == 1) {
 
 				int parColor = parent->color;
 
-				//1.�������ֵܵ��ҽ��
+				//1.先左旋兄弟的右结点
 				sibling = leftRotate(sibling->right);
 
-				// 2.�������ֵ�
+				// 2.再右旋兄弟
 				parent = rightRotate(sibling);
 
-				// �����ת���Ƿ�Ϊ��
+				// 检查旋转点是否为根
 				if (parent->par == NULL) {
 					*root = parent;
 				}
 
-				// 3.�ı���ɫ
+				// 3.改变颜色
 				parent->color = parColor;
 				parent->left->color = 0;
 				parent->right->color = 0;
 
-				// 4.�ѽ��ɾ��
+				// 4.把结点删除
 				if (Delete) {
 					if (toDelete->left != NULL) {
 						toDelete->left->par = parent->right;
@@ -381,24 +381,24 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 				}
 
 			}
-			else {   //�ֵ�Ϊ���׵����ӽ�㣬 �ֵ�Ҳ���ڵ����ӽ��
+			else {   //兄弟为父亲的右子结点， 兄弟也存在的右子结点
 
 				int parColor = parent->color;
 
-				// 1.�������ֵܽ��
+				// 1.先左旋兄弟结点
 				parent = leftRotate(sibling);
 
-				//�����ת���Ƿ�Ϊ��
+				//检查旋转点是否为根
 				if (parent->par == NULL) {
 					*root = parent;
 				}
 
-				// ������ɫ
+				// 更改颜色
 				parent->color = parColor;
 				parent->left->color = 0;
 				parent->right->color = 0;
 
-				// 2.ɾ�����
+				// 2.删除结点
 				if (Delete) {
 					if (toDelete->right != NULL) {
 						toDelete->right->par = parent->left;
@@ -411,29 +411,29 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 		}
 		else {
 
-			// �ֵ�Ϊ���׵����ӽ�㣬�ֵܺ���Ϊ���ӽ�� 
+			// 兄弟为父亲的右子结点，兄弟孩子为左子结点 
 			if (locateChild == 0) {
 
-				int parColor = parent->color;   //��¼���׵���ɫ
+				int parColor = parent->color;   //记录父亲的颜色
 
-				//  1�� �����ֵܵ����ӽ��
+				//  1。 右旋兄弟的左子结点
 				sibling = rightRotate(sibling->left);
 
 
-				// 2.�����ֵܽ��
+				// 2.左旋兄弟结点
 				parent = leftRotate(sibling);
 
-				// �����ת���Ƿ�Ϊ�����
+				// 检查旋转点是否为根结点
 				if (parent->par == NULL) {
 					*root = parent;
 				}
 
-				// ������ɫ
+				// 更改颜色
 				parent->color = parColor;
 				parent->left->color = 0;
 				parent->right->color = 0;
 
-				// 3.ɾ�����
+				// 3.删除结点
 				if (Delete) {
 					if (toDelete->right != NULL) {
 						toDelete->right->par = parent->left;
@@ -444,24 +444,24 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 
 
 			}
-			else {   //�ֵ�Ϊ���ӽ�㣬�ֵܵĺ���ҲΪ���ӽ��
+			else {   //兄弟为左子结点，兄弟的孩子也为左子结点
 
 				int parColor = parent->color;
 
-				// �����ֵ�
+				// 右旋兄弟
 				parent = rightRotate(sibling);
 
-				// �����ת���Ƿ�Ϊ��
+				// 检查旋转点是否为根
 				if (parent->par == NULL) {
 					*root = parent;
 				}
 
-				// ������ɫ 
+				// 更改颜色 
 				parent->color = parColor;
 				parent->left->color = 0;
 				parent->right->color = 0;
 
-				//ɾ�����
+				//删除结点
 				if (Delete) {
 					if (toDelete->left != NULL) {
 						toDelete->left->par = parent->right;
@@ -473,21 +473,21 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 			}
 		}
 	}
-	else if (sibling->color == 0) {						 //����ֵܵ���ɫΪ��ɫ
+	else if (sibling->color == 0) {						                //如果兄弟的颜色为黑色
 
-		// �ı�Ϊ��ɫ
+		// 改变为红色
 		sibling->color = 1;
 
 		
 		if (Delete) { 
-			if (locateChild) {							//ɾ������λ���ڸ������ұ�
+			if (locateChild) {							//删除结点的位置在父结点的右边
 				toDelete->par->right = toDelete->left;
 				if (toDelete->left != NULL) {
 					toDelete->left->par = toDelete->par;
 				}
 			}
 			else {
-				toDelete->par->left = toDelete->right;  //ɾ������λ���ڸ��������
+				toDelete->par->left = toDelete->right;  			//删除结点的位置在父结点的左边
 				if (toDelete->right != NULL) {
 					toDelete->right->par = toDelete->par;
 				}
@@ -497,7 +497,7 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 		checkForCase2(parent, 0, locateChild, root);           
 	}   
 	else {   
-		if (locateChild) {    //����
+		if (locateChild) {    //右旋
 
 			toDelete->par->right = toDelete->left;
 			if (toDelete->left != NULL) {
@@ -506,7 +506,7 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 
 			parent = rightRotate(sibling);
 
-			//�ж���ת���Ƿ�Ϊ�����
+			//判断旋转点是否为根结点
 			if (parent->par == NULL) {
 				*root = parent;
 			}
@@ -515,7 +515,7 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 			parent->right->color = 1;
 			checkForCase2(parent->right, 0, 1, root);
 		}
-		else {   // ����
+		else {   // 左旋
 
 			toDelete->par->left = toDelete->right;
 			if (toDelete->right != NULL) {
@@ -523,7 +523,7 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 			}
 			parent = leftRotate(sibling);
 
-			//�ж���ת���Ƿ�Ϊ�����
+			//判断旋转点是否为根结点
 			if (parent->par == NULL) {
 				*root = parent;
 			}
@@ -536,11 +536,11 @@ void checkForCase2(Node* toDelete, int Delete, int fromDirection, Node** root) {
 
 }
 
-//���Ľ��ɾ��
+//树的结点删除
 void deleteNode(int val, Node** root) {
 	Node* buffRoot = *root;
 
-	//���ҵ�Ҫɾ���Ľ��
+	//先找到要删除的结点
 	while (1) {
 
 		if (val == buffRoot->val) 
@@ -548,7 +548,7 @@ void deleteNode(int val, Node** root) {
 			break;
 		}
 
-		if (val > buffRoot->val) {                //���ҳ�������
+		if (val > buffRoot->val) {                //向右持续查找
 			if (buffRoot->right != NULL) {
 				buffRoot = buffRoot->right;
 			}
@@ -558,7 +558,7 @@ void deleteNode(int val, Node** root) {
 			}
 		}
 		else {
-			if (buffRoot->left != NULL) {        //�����������
+			if (buffRoot->left != NULL) {        //向左持续查找
 				buffRoot = buffRoot->left;
 			}
 			else {
@@ -570,7 +570,7 @@ void deleteNode(int val, Node** root) {
 
 	Node* toDelete = buffRoot;
 
-	// �鿴����߽�����Ҷ�ӽ�㣬�����ұ߽�� ����Ҷ�ӽ��
+	// 查看最左边结点的右叶子结点，和最右边结点 的左叶子结点
 	if (toDelete->left != NULL) {
 		toDelete = toDelete->left;
 		while (toDelete->right != NULL) {
@@ -588,16 +588,16 @@ void deleteNode(int val, Node** root) {
 		return;
 	}
 
-	// ����ֵ
+	// 交换值
 	buffRoot->val = toDelete->val;
 	toDelete->val = val;
 
-	// ����Ƿ�Ϊ��һ�����
+	// 检查是否为第一种情况
 	if (toDelete->color == 1 || (toDelete->left != NULL && toDelete->left->color == 1) || (toDelete->right != NULL && toDelete->right->color == 1)) {
 
-		// �����Ҷ�ӽ��
+		// 如果是叶子结点
 		if (toDelete->left == NULL && toDelete->right == NULL) {
-			// ֱ��ɾ��
+			// 直接删除
 			if (toDelete->par->left == toDelete) {
 				toDelete->par->left = NULL;
 			}
@@ -605,16 +605,16 @@ void deleteNode(int val, Node** root) {
 				toDelete->par->right = NULL;
 			}
 		}
-		else {   //���ɾ�����ĺ��ӽ��Ϊ��ɫ
+		else {   //如果删除结点的孩子结点为红色
 
-			//����Ƿ��������
+			//检查是否存在左孩子
 			if (toDelete->left != NULL) {
-				//����������Ϊɾ�����µĸ����
+				//把左孩子提升为删除后新的父结点
 				toDelete->par->right = toDelete->left;
 				toDelete->left->par = toDelete->par;
 				toDelete->left->color = 1;
 			}
-			else {   //���Һ�������Ϊɾ������µĸ����
+			else {   //把右孩子提升为删除后的新的父结点
 				toDelete->par->left = toDelete->right;
 				toDelete->right->par = toDelete->par;
 				toDelete->right->color = 1;
@@ -624,7 +624,7 @@ void deleteNode(int val, Node** root) {
 		
 		free(toDelete);
 	}
-	else {   // ���2 
+	else {   // 情况2 
 		checkForCase2(toDelete, 1, ((toDelete->par->right == toDelete)), root);
 	}
 
@@ -633,7 +633,7 @@ void deleteNode(int val, Node** root) {
 }
 
 
-//�����ӡ���Ľ��ֵ����ɫ
+//中序打印树的结点值和颜色
 void printInorder(Node* root) {
 	if (root != NULL) {
 		printInorder(root->left);
@@ -644,7 +644,7 @@ void printInorder(Node* root) {
 
 
 
-//������к�ɫ�ĸ���
+//检查树中黑色的个数
 void checkBlack(Node* temp, int c) {
 	if (temp == NULL) {
 		printf("%d ", c);
