6837827700138dc1096fe9d5855f20a1ccbadc6e
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-24 13:22:06 +0800

    Update 插值搜索查找法(interPolationSearch).cpp

--- Searching/插值搜索查找法(interPolationSearch).cpp
+++ Searching/插值搜索查找法(interPolationSearch).cpp
@@ -8,7 +8,7 @@ int interPolationSearch(int arr[], int n, int key)
 	int low = 0, high = n - 1;
 	while (low<= high && key >= arr[low] && key<= arr[high])
 	{
-		//用线性函数来计算出最可能接近的位置
+		//用线性函数来计算出最可能接近的位置 (函数可由自己来设定)
 		int pos = low + ((key - arr[low]) * (high - low)) / (arr[high] - arr[low]);
 		if (key > arr[pos])
 			low = pos + 1;
