d9c0a9a53db219b1efe754c08c30f178bb6075b4
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-24 20:09:13 +0800

    Update and rename KPM.cpp to 字符串匹配算法/KPM.cpp

--- 字符串匹配算法/KPM.cpp
+++ 字符串匹配算法/KPM.cpp
@@ -8,7 +8,7 @@ bool KMP2(string s1, string s2);
 
 int main()
 {
-	string s1 = "adadffadad";
+	string s1 = "adadffadad";      			//测试用例
 	string s2 = "ffd";
 
 	cout << s1 << endl;
@@ -20,17 +20,17 @@ int main()
 }
 
 
-void getNext(string s, int * next1) //传入一个模式串s 和一个next数组
+void getNext(string s, int * next1)			 //传入一个模式串s 和一个next数组
 {
 	
 	next1[0] = -1;
 	int i = 0, j = -1;
 
-	while (i < s.length())
+	while (i < s.length())				
 	{
 		if ((j == -1) || (s[i] == s[j]))
 		{
-			++i;							//注意i是一直增加的
+			++i;				//注意i是一直增加的
 			++j;
 			next1[i] = j;
 			
@@ -47,7 +47,7 @@ void getNext(string s, int * next1) //传入一个模式串s 和一个next数组
 
 
 
-int KMP(string s1, string s2)  //s1为主串  s2为模式串
+int KMP(string s1, string s2)  				//s1为主串  s2为模式串
 {
 	int size = s2.length();
 	int* next = new int[size];
@@ -56,7 +56,7 @@ int KMP(string s1, string s2)  //s1为主串  s2为模式串
 	cout << s1.length();
 	for (int i = 0; i < s1.length(); i++)
 	{
-		if (k > -1 && s2[k + 1] != s1[i])		//主串和模式串不相同，就把k回溯，
+		if (k > -1 && s2[k + 1] != s1[i])	//主串和模式串不相同，就把k回溯，
 		{
 			k = next[k];
 		}
@@ -76,7 +76,7 @@ int KMP(string s1, string s2)  //s1为主串  s2为模式串
 
 
 
-bool KMP2(string s1, string s2)    //s1代表主串  s2代表模式串
+bool KMP2(string s1, string s2)   		 //s1代表主串  s2代表模式串
 {
 	//int len2 = s2.length();
 	int * next2 = new int ;
