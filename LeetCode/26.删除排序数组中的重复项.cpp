8f51e63d3ac383c73f1dfd876f243b71a71d494f
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-26 12:21:36 +0800

    Update 26.删除排序数组中的重复项.cpp

--- LeetCode/26.删除排序数组中的重复项.cpp
+++ LeetCode/26.删除排序数组中的重复项.cpp
@@ -1,3 +1,4 @@
+
 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
 
 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
@@ -26,22 +27,21 @@
 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
 
 
-
+//使用双指针的方法
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         std::vector<int>::iterator p1 =nums.begin(), p2 =nums.begin()+1;
-        std::vector<int>::iterator 
          int s = nums.size();
          if(s==0 || s == 1)
           { return s;}
          while(p2 != nums.end())
          {   
-            if(*p1 == *p2)
-            { nums.erase(p2);
+            if(*p1 == *p2)               //遇到相同的就把p2指向的值删去
+            { nums.erase(p2);               
               s--;
             }
-           else
+           else                          //不同就前进一步
             {
               p1++;p2++;
             }
