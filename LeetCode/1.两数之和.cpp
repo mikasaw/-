784ae230f4ecb96168f8a9cf5d4c40c29e0e76cf
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-26 11:52:37 +0800

    Update 1.两数之和.cpp

--- LeetCode/1.两数之和.cpp
+++ LeetCode/1.两数之和.cpp
@@ -1,13 +1,13 @@
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 /整数，并返回他们的数组下标。
 
-你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+//你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
 
-示例:
+//示例:
 
-给定 nums = [2, 7, 11, 15], target = 9
+//给定 nums = [2, 7, 11, 15], target = 9
 
-因为 nums[0] + nums[1] = 2 + 7 = 9
-所以返回 [0, 1]
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
 
 
 class Solution {
@@ -15,12 +15,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) 
     {  
       vector<int>a;
-      unordered_map<int,int>ha;
-     for(int i=0;i<nums.size();i++)  
-    {
-       ha[nums[i]]=i;
+      unordered_map<int,int>ha;     
+      for(int i=0;i<nums.size();i++)      //在与数组数值对于的hashmap里存下数值元素的下标，便于寻找。
+     {
+       ha[nums[i]]=i;           
        int  s=target-nums[i];
-       if(ha.count(s))
+       if(ha.count(s))                    //如果s存在，则找到答案。
        {
        a=vector<int>{ha[s],i};
        return a; 
