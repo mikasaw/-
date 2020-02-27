6ffef9dc262b7f630ea30e66d75b7045da639403
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-27 16:37:38 +0800

    Update 303.区域和检索-数组不可变.cpp

--- LeetCode/动态规划题/303.区域和检索-数组不可变.cpp
+++ LeetCode/动态规划题/303.区域和检索-数组不可变.cpp
@@ -38,3 +38,28 @@ private:
 
 
 
+2.//用缓冲换时间的方法
+    class NumArray {
+public:
+     
+    NumArray(vector<int>& nums): num(nums) {
+    
+       for(int i= 1;i<num.size();i++)
+       {
+         num[i] = num[i]+num[i-1];
+       }
+    }
+    
+    int sumRange(int i, int j)
+    {
+        if(i == 0)
+        return num[j];
+        else 
+        return num[j]-num[i-1];
+    }
+
+private:
+     vector<int >& num;
+};
+
+
