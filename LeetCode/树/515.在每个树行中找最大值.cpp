f3f7d2b3ef5b706df118f74080f8926ca19f7c36
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-02-28 17:00:19 +0800

    Create 515.在每个树行中找最大值.cpp

---
+++ LeetCode/树/515.在每个树行中找最大值.cpp
@@ -0,0 +1,47 @@
+/*
+您需要在二叉树的每一行中找到最大的值。
+示例：
+
+输入: 
+
+          1
+         / \
+        3   2
+       / \   \  
+      5   3   9 
+
+输出: [1, 3, 9]
+通过次数7,258提交次数12,405
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+//在BFS的基础上再把每行中的最大值存起来
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+       vector<int> ans;
+        if(root == NULL)
+         return ans;
+         queue<TreeNode*>q;
+         q.push(root);
+        
+        while(!q.empty())
+        {
+            int max1 =INT_MIN;
+            int size = q.size();
+            for(int i=0;i<size;i++)                       //遍历树的其中一行
+            {    
+                 TreeNode* cur = q.front();
+                 q.pop();
+                 if(cur->left) q.push(cur->left);
+                 if(cur->right) q.push(cur->right);
+                 max1 = max(max1,cur->val);               //找出行中的最大值           
+            }
+            ans.push_back(max1);                          //把最大值存入结果数组中 
+        }
+        return ans;
+    }
+};
