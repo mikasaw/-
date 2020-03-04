4277e7edbccf97a42e6e8d72e90fd0a540e59d8b
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-03-04 21:15:20 +0800

    Update 994.腐烂的橘子.cpp

--- LeetCode/994.腐烂的橘子.cpp
+++ LeetCode/994.腐烂的橘子.cpp
@@ -1,3 +1,4 @@
+/*
 在给定的网格中，每个单元格可以有以下三个值之一：
 
 值 0 代表空单元格；
@@ -32,7 +33,56 @@ grid[i][j] 仅为 0、1 或 2
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/rotting-
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
 
+
+class Solution {
+public:
+        int cnt = 0;
+        int dis[10][10];
+        int dir_x[4] = {0,1,0,-1};
+        int dir_y[4] = {1,0,-1,0};
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int,int>>q;
+        memset(dis,-1,sizeof(dis));
+        cnt = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans = 0;
+        for(int i = 0.;i<n;i++)
+        {
+            for(int j = 0; j < m ;j++)
+            {
+                if(grid[i][j] == 2)
+                {
+                    q.push(make_pair(i,j));
+                    dis[i][j]= 0;
+                }
+                else if(grid[i][j] == 1) cnt+=1;
+            }
+        }
+        while(!q.empty())
+        {
+            pair<int,int> x = q.front();
+            q.pop();
+            for(int i = 0;i<4;i++)
+            {
+                int mx =x.first + dir_x[i];
+                int my= x.first + dir_y[i];
+                if(mx<0 || mx >=n || my<0 || my>m || ~dis[mx][my] || !grid[mx][my] ) continue;
+                dis[mx][my] = dis[x.first][x.second]+1;
+                q.push(make_pair(mx,my));
+                if(grid[mx][my] == 1)
+                 {
+                     cnt -= 1;
+                     ans = dis[mx][my];
+                     if(!cnt) break;
+                 }
+            }
+        }
+        return cnt ? -1:ans;
+    }
+};
 
 
 
