aace20aa1211974ea4c6e9a1851c6323717c6dc7
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-03-10 17:53:39 +0800

    Create 50.Pow(x,n)实现.cpp

---
+++ LeetCode/50.Pow(x,n)实现.cpp
@@ -0,0 +1,47 @@
+实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+
+示例 1:
+
+输入: 2.00000, 10
+输出: 1024.00000
+示例 2:
+
+输入: 2.10000, 3
+输出: 9.26100
+示例 3:
+
+输入: 2.00000, -2
+输出: 0.25000
+解释: 2-2 = 1/22 = 1/4 = 0.25
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/powx-n
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+
+
+
+//暴力法
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x == 1 || n ==1)
+        return x;
+        if(n == 0)
+        return 1;
+          long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        for (long long i = 0; i < N; i++)
+            ans = ans * x;
+        return ans;
+    }
+};
+
+
+
+
