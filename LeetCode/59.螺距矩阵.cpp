e7a984062a1fd08b6695222d4638e42e0ccbd54e
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-03-14 13:03:45 +0800

    Update 59.螺距矩阵.cpp

--- LeetCode/59.螺距矩阵.cpp
+++ LeetCode/59.螺距矩阵.cpp
@@ -15,4 +15,50 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 
-//在前面54
+//在前面54题的基础上进行结题
+//把取出变成放入
+
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+      vector<vector<int>>ans(n);
+      for(int i = 0;i<ans.size();i++)
+      {
+          ans[i].resize(n);
+      }
+     int upper = 0, downer = n-1;
+     int left = 0,right = n-1;
+     int num = 1;
+     while(upper <= downer && left <= right)
+     {
+         for(int i = left;i<= right ;i++)
+         {
+             ans[upper][i] = num++;
+         }
+         upper++;
+         if(upper > downer)  break;
+
+         for(int i = upper;i<=downer;i++)
+         {
+             ans[i][right]=num ++;
+         }
+         right --;
+         if(left > right) break;
+
+        for(int i = right;i>=left;i--)
+         {
+            ans[downer][i]= num++;
+         }
+        downer--;
+
+        for(int i = downer;i>=upper;i--)
+        {
+            ans[i][left] = num++;
+        }
+        left++;
+
+     }
+     return ans;
+
+    }
+};
