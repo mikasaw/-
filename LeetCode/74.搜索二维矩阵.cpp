55360cea858f729ab201e431f958256831ce9791
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-03-16 00:18:49 +0800

    Create 74.搜索二维矩阵.cpp

---
+++ LeetCode/74.搜索二维矩阵.cpp
@@ -0,0 +1,61 @@
+编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+
+每行中的整数从左到右按升序排列。
+每行的第一个整数大于前一行的最后一个整数。
+示例 1:
+
+输入:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+输出: true
+示例 2:
+
+输入:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+输出: false
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/search-a-2d-matrix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+//先比较每个vector里每列的头元素，找到准确的一行再与整行进行比较
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() == 0) return false;
+        if(matrix[0].size() == 0) return false; 
+        int  c= 0;
+        for(int i = 1;i<matrix.size();i++)
+        {
+            if(matrix[i][0] > target)
+             {
+                 c = i-1;
+                 break;
+             }
+            else if(matrix[i][0] == target)
+            { 
+                return true;
+            }
+            else  
+              {
+                  c++;
+                  continue;
+              }
+        }
+        for(int j = 0;j<matrix[c].size() ;j++)
+        {
+            if(matrix[c][j] == target) return true;
+        }
+        return false;
+    }
+};
