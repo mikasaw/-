c8b0aee663dd1bb7532f8b7e5e15dba48cd6995b
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-03-25 10:11:54 +0800

    Update 36.有效的数独.cpp

--- LeetCode/36.有效的数独.cpp
+++ LeetCode/36.有效的数独.cpp
@@ -70,3 +70,33 @@ public:
         return true;
     }
 };
+
+
+//使用数组的方法
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<int>> row (9, vector<int>(9,0));
+        vector<vector<int>> col (9, vector<int>(9,0));
+        vector<vector<int>> box (9, vector<int>(9,0));
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.'){
+                    continue;
+                }
+                int val = board[i][j] - '1';
+                int box_index = (i/3) * 3 + j/3;
+                if(row[i][val] == 0 && col[j][val] == 0 && box[box_index][val] == 0){
+                    row[i][val] = 1;
+                    col[j][val] = 1;
+                    box[box_index][val] = 1;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
