2e5c1e302c44562a0ce3a44db8d18ced21f58557
mikasaw <57830391+mikasaw@users.noreply.github.com>
2020-04-02 14:27:57 +0800

    Update 64.最小路径和.cpp

--- LeetCode/64.最小路径和.cpp
+++ LeetCode/64.最小路径和.cpp
@@ -39,3 +39,25 @@ public:
        return dp[0][0];
     }
 };
+
+
+//一维数组的动态规划
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+       vector<int>dp(grid[0].size());
+       for(int i = grid.size()-1;i>=0;i--){
+           for(int j = grid[0].size()-1;j>=0;j--){
+               if(i==grid.size()-1 && j!=grid[0].size()-1)
+                  dp[j]=grid[i][j] + dp[j+1];
+               else if(j==grid[0].size()-1 && i!=grid.size()-1)
+                  dp[j]=grid[i][j]+dp[j];
+               else if(j!=grid[0].size()-1 && i!=grid.size()-1)
+                  dp[j]=grid[i][j] + min(dp[j] ,dp[j+1]);
+               else 
+                  dp[j] = grid[i][j];
+           }
+       }
+       return dp[0];
+    }
+};
